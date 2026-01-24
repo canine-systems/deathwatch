b6d722eb2123aa66385f72eb551629d6dae9ed8e
Ellen Marie Dash <me@duckie.co>
2026-01-23 21:35:01 -0500

    text/http -> text/html

--- src/main/java/com/kitsuneindustries/deathwatch/web/DeathwatchServlet.java
+++ src/main/java/com/kitsuneindustries/deathwatch/web/DeathwatchServlet.java
@@ -10,13 +10,12 @@ import jakarta.servlet.http.HttpServletResponse;
 
 @WebServlet("/*")
 public class DeathwatchServlet extends HttpServlet {
-
     private static final long serialVersionUID = -7539935506921331750L;
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
         throws ServletException, IOException {
-        response.setContentType("text/http");
+        response.setContentType("text/html");
         response.setStatus(HttpServletResponse.SC_OK);
         response.getWriter().println("<h1>Hello from DeathwatchServlet</h1>");
     }
